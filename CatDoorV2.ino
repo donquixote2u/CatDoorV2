a82fbbdf18a4b8db02afd369110b43bc4a2e23eb
donquixote2u <donquixote2u@gmail.com>
2019-11-14 11:19:33 +1300

    modified:   CatDoorV2.ino

--- CatDoorV2.ino
+++ CatDoorV2.ino
@@ -7,21 +7,28 @@
 #ifdef UNO
 #define SENSEPIN 2
 #define SERVOLINE 3
+#define RX1 8
+#define TX1 9
 #endif
 #define SerialRate 115200
-#define Ser2Rate 57600
-#include <AltSoftSerial.h>  // reserves pins 8(Rx)9(Tx)
-Altsoftserial RFI;         // set up second serial line for HZ1050
+#define Ser2Rate 19200
+#define INDEX_SIZE 48 // buffer size set to 48 char 
+#include <SoftwareSerial.h> // reserves pins 8(Rx)9(Tx)
+SoftwareSerial RFin(RX1,TX1);         // set up second serial line for HZ1050
 #include <ServoTimer2.h>
 ServoTimer2 myservo;  // create servo object to control a servo
 int pos;        // desired position of servo
+char rxbuffer[INDEX_SIZE] = {}; //  receive buffer
+int buffptr = 0; // position in circular buffer above
+long ID=0;        // tag ID read
 int DoorState, lastDoorState;	// state of Hall effect sensor on door
 int lastTriggerValue=0;	   	// stored trigger value to detect change
 int Mode="B";				        // stores door status mode (I,O,B,L)
 bool Debug=true;            // debug (serial output) mode switch
+
 void setup()
 {
-   RFI.begin(Ser2Rate);       // set up RFI serial comms
+   RFin.begin(Ser2Rate);       // set up RFI serial comms
    pinMode(SENSEPIN, INPUT);  // door state sensor
    DoorState = HIGH;
    lastDoorState = DoorState;    // init door state change = same = NO
@@ -30,48 +37,48 @@ void setup()
    doorlatch(pos);              // home servo
 }
 
-void DebugOut(String txt) {
-  if(Debug) 
-  Serial.print(txt);
-}
-
 void loop()
 {
    DoorState = digitalRead(SENSEPIN);
    if (DoorState != lastDoorState) {
-     Serial.print("\nDoor state change:"); 
    // if the state has changed and change is to LOW door is closed
    if (DoorState == LOW) {
-        Serial.print("Door closed"); 
-	if(Mode!="B")
+        Serial.print("\nDC"); 
+	 if(Mode!="B")
            { pos=2100; doorlatch(pos); }     //if door closed, ensure latch also closed
         }
      else { 
-        Serial.print("Door open"); 
+        Serial.print("\nDO"); 
         pos=1100; doorlatch(pos);             //if door open, ensure latch also open
       	}
      lastDoorState=DoorState;        // store latest door state   
     } 
     checkControls();                  // any control commands received?     
     buffptr=0;
-    while(RFin.available()>0)
+   if(RFin.available()>0)
+     {
+      buffptr=0; ID=0;
+      while(RFin.available()>0)
        {
         rxbuffer[buffptr]=RFin.read();
+        buffptr++;
        }              // end while serial
-    DebugOut("READ IN:\n");   
-    for(int i=0;i<=buffptr;i++)
-      {     
-       DebugOut(rxbuffer[i],HEX);
-      }
-    DebugOut('\n');  
-    ID=strtol(rxbuffer,NULL,16); 
-    DebugOut(ID,DEC);
-    DebugOut('\n');  
-    // if(ID==tag1 || ID==tag2)  // if authoried tag detected, 
-      {
+      for(int i=0;i<buffptr;i++)
+        {     
+          // DEBUG Serial.write(rxbuffer[i]);
+          ID <<= 8;      // shift left 8 bits, add new byte to ID number
+          ID |= rxbuffer[i];
+        }
+      Serial.print("\nID");
+      Serial.print(ID,DEC);
+      // if(ID==tag1 || ID==tag2)  // if authoried tag detected, 
+      // {
       // pos=1100; doorlatch(pos); delay(15000); pos=2150; doorlatch(pos);
-      }  
- }
+      // } 
+      // else { Serial.print("\nunidentified tag"); }
+      }
+    delay(500); 
+}
 
 void checkControls() {
 int data;
@@ -97,11 +104,11 @@ bool cmdmode=false;
           pos=2150; doorlatch(pos);
           break;
         }         // end switch
-	Serial.print("status "+Mode);
+	Serial.print("\n#M"+Mode);
       }           // end cmdmode
     else { if(data==35)			// "#" precedes a single-char mode command
              { 
-              Serial.println("\ncommand mode on");
+              // DEBUG Serial.println("\ncommand mode on");
               cmdmode=true; 
              }
            else
